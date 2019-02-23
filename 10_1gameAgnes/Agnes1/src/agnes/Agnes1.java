4ed750cbbdd05b16dab23d3efee94aef274c9d6d
Chong-wei Xu <34006571+cxuksu@users.noreply.github.com>
2019-02-23 14:35:51 -0500

    Add files via upload

---
+++ 10_1gameAgnes/Agnes1/src/agnes/Agnes1.java
@@ -0,0 +1,30 @@
+/*
+ * Agnes1.java - The main class of the game Agnes
+ */
+package agnes;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author cxu
+ */
+public class Agnes1 extends JFrame {
+
+    public Agnes1() {
+        setTitle("Game Agnes");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(Consts.CV_WIDTH, Consts.CV_HEIGHT);
+        
+        GameCanvas gameCv = new GameCanvas();
+        add(gameCv);
+        
+        setVisible(true);
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Agnes1();
+    }
+}
---
+++ 10_1gameAgnes/Agnes1/src/agnes/Deck.java
@@ -0,0 +1,72 @@
+/*
+ * Deck.java - A class defines a deck of cards.
+ */
+package agnes;
+
+import java.awt.Image;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author cxu
+ */
+public class Deck {
+
+    private ArrayList<Card> deck;
+    private Card card;
+
+    public Deck() {
+        deck = new ArrayList();
+        initDeck();
+        shuffleCard();
+    }
+
+    private void initDeck() { // deck is sorted
+        deck.clear();
+        for (int i = 0; i < Consts.DECK_LEN; i++) {
+            card = initNewCard(i);
+            deck.add(card);
+        }
+    }
+    
+    public Card initNewCard(int idx) {
+        Image img = null;
+        try {
+            img = ImageIO.read(getClass().getResource(
+                    "../cards/" + (idx+1) + ".gif"));
+        } catch (Exception e) {
+        }
+        Card aCard = new Card();
+        aCard.setCardFront(img);
+
+        return aCard;
+    }
+
+    private void shuffleCard() {
+        int randIdx1, randIdx2;
+        for (int i = 0; i < 1000; i++) {
+            randIdx1 = ((int)(Math.random() * 10000)) % Consts.DECK_LEN;
+            randIdx2 = ((int)(Math.random() * 10000)) % Consts.DECK_LEN;
+            swapCard(randIdx1, randIdx2);
+        }
+    }
+    
+    public void swapCard(int idx1, int idx2) {
+        Card tCard;
+        tCard = deck.get(idx1);
+        deck.set(idx1, deck.get(idx2));
+        deck.set(idx2, tCard);
+    }
+    
+    public Card dealCard() {
+        Card theCard;
+        theCard = deck.get(0);
+        deck.remove(0);
+        return theCard;
+    }
+    
+    public ArrayList<Card> getDeck() {
+        return deck;
+    }
+}
